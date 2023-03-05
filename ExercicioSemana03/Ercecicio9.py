3fad55975fc56981ed990cd7349eadd47ebf6e55
Nicollas Willian Amaral da Silva <67935302+NikcWill@users.noreply.github.com>
2023-03-05 17:59:41 -0300

    atualizando

---
+++ ExercicioSemana03/Ercecicio9.py
@@ -0,0 +1,14 @@
+import Exercicio4
+
+nome = ['Ana', 'Bruno', 'Carla', 'Daniel', 'Elisa']
+
+print("A lista adicionada segue abaixo: ")
+Exercicio4.list.append(nome)
+
+print(Exercicio4.list)
+'''for i in nome:
+     Exercicio4.list.append(i)
+print(Exercicio4.list)'''
+
+
+
