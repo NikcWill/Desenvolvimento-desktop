1f00d4c1aa05577a6123b0b5f2cf3584260689e4
Nicollas Willian Amaral da Silva <67935302+NikcWill@users.noreply.github.com>
2023-03-08 14:44:44 -0300

    finalizado

---
+++ ExercicioSemana03/Exercicio14.py
@@ -0,0 +1,30 @@
+from Exercicio11 import validador
+
+listaNomes = ["Ana", "João", "Maria", "Pedro", "Sofia"]
+lista = validador()
+
+
+def montadorLista (lista1, lista2):
+    novaLista = []
+    for i in lista1:
+        if type(i) == list:
+            novaLista.append(montadorLista(i))
+        elif type(i) != str:
+            novaLista.append(str(i))
+        else:
+            novaLista.append(i)
+    for i in lista2:
+        if type(i) == list:
+            novaLista.append(montadorLista(i))
+        elif type(i) != str:
+            novaLista.append(str(i))
+        else:
+            novaLista.append(i)
+    return novaLista
+
+
+resultado = montadorLista(lista, listaNomes)
+
+strmontada = ' '.join(str(x) for x in resultado)
+print(strmontada)
+print(type(strmontada))
