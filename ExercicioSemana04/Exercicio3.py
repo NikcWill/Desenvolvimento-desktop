e81616e0dead14712109a9088eca61c2e25a7cbc
Nicollas Willian Amaral da Silva <67935302+NikcWill@users.noreply.github.com>
2023-03-11 23:56:41 -0300

    Exercicio 13 feito com o validador de qualquer numero funcionando

--- ExercicioSemana04/Exercicio3.py
+++ ExercicioSemana04/Exercicio3.py
@@ -1,21 +1,31 @@
+import re
+
+inteiros = re.compile(r'^[1-9]\d*$')
+quebrados = re.compile(r'^\b\d+\.\d+\b$')
+
+
 class Retangulo:
     def __init__(self, base, altura):
-        self.base = Retangulo.validador(base)
-        self.altura = Retangulo.validador(altura)
+        self.base = base
+        self.altura = altura
 
     def calculador(self):
         i = self.base * self.altura
-        return i
+        print('A area do retangulo é: ')
+        return float(i)
+
 
     @staticmethod
-    def validador(numero):
+    def validador():
         while True:
-           if type(numero) == int or type(numero) == float:
-                return numero
+            n = input(' Escreva o numero: ')
+            if inteiros.findall(n):
+                return int(n)
+            elif quebrados.findall(n):
+                return float(n)
             else:
                 print('Numero preenchido incorreto')
 
 
-
-
-
+r = Retangulo(Retangulo.validador(), Retangulo.validador())
+print(r.calculador())
