a7cd4ac42755a5b145443f0c38e78f9b7cc04ddf
Nicollas Willian Amaral da Silva <67935302+NikcWill@users.noreply.github.com>
2023-03-21 20:29:34 -0300

    Criando o primeiro QT

---
+++ QTdesktop/main.py
@@ -0,0 +1,14 @@
+import sys
+from PySide6 import *
+from PySide6.QtWidgets import QMainWindow, QApplication
+
+
+class MainWindow (QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+app = QApplication(sys.argv)
+principal = MainWindow()
+principal.show()
+app.exec()
